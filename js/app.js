36074ac0c7c8a8deda42e92abcda88a95a28ed92
Jakub <jakub.szaredko@gmail.com>
2021-06-18 22:56:20 +0200

    Create game over screen, add new animations

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 let playersTurn = 'x';
+let gameOver = false;
 const game = ['', '', '', '', '', '', '', '', ''];
 const winConditions = [
     [0, 1, 2],
@@ -12,31 +13,49 @@ const winConditions = [
 ];
 
 const board = document.querySelector('.tic-tac-toe__board');
+const gameOverContainer = document.querySelector('.tic-tac-toe__game-over');
 
 const createMark = (target, mark) => {
     const m = document.createElement('span');
-    m.setAttribute('class', `tic-tac-toe__mark ${mark}`);
-    target.appendChild(m);
+    m.setAttribute('class', `tic-tac-toe__mark ${mark} animated`);
+    const ma = target.appendChild(m);
+    setTimeout(() => ma.classList.remove('animated'), 50);
 };
 
-const isGameOver = () => {
+const isGameOver = callback => {
     for (const winCondition of winConditions) {
         const a = game[winCondition[0]];
         const b = game[winCondition[1]];
         const c = game[winCondition[2]];
 
         if (a === '' || b === '' || c === '') continue;
-        if (a === b && a === c) return true;
+        if (a === b && a === c) {
+            callback(winCondition[0], winCondition[1], winCondition[2]);
+            return true;
+        }
     }
 
     return false;
 };
 
-//const validateLine = (a, b, c) => /^(x|o)$/.test(game[a]) && game[a] === game[b] && game[a] === game[c];
+const handleGameOver = (a, b, c) => {
+    gameOver = true;
+
+    Array.from(board.querySelectorAll('.tic-tac-toe__cell'))
+        .filter((cell, i) => i !== a && i !== b && i !== c)
+        .forEach(cell => {
+            const mark = cell.querySelector('.tic-tac-toe__mark');
+            console.log(mark);
+            if (mark) mark.classList.add('test');
+        });
+
+    setTimeout(() => gameOverContainer.classList.add('active'), 3000);
+};
 
 board.addEventListener('click', e => {
+    if (gameOver) return;
+
     const t = e.target;
-    console.log(e);
 
     if (t.classList.contains('tic-tac-toe__cell') && !t.classList.contains('filled')) {
         createMark(t, playersTurn);
@@ -45,6 +64,6 @@ board.addEventListener('click', e => {
         game[Number.parseInt(t.getAttribute('data-index'))] = playersTurn;
         playersTurn = playersTurn === 'x' ? 'o' : 'x';
 
-        console.log(isGameOver());
+        isGameOver(handleGameOver);
     }
 });
